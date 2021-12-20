e778efe3023b28bd447ccb092554de0e296bc92a
GroupRec <85758145+GroupRec@users.noreply.github.com>
2021-12-20 11:23:30 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,11 +1,3 @@
-'''
-Created on Aug 8, 2016
-Processing datasets.
-
-@author: Xiangnan He (xiangnanhe@gmail.com)
-
-Modified  on Nov 10, 2017, by Lianhai Miao
-'''
 
 import scipy.sparse as sp
 import numpy as np
